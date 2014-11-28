308aab1d4a0ea486f9c5fdaeeae809dab2cc2723
Arie Miller <renari@arimil.com>
2014-11-28 18:47:04 -0500

    weaknesses now output in teambuilder

--- pokeweakness.user.js
+++ pokeweakness.user.js
@@ -4,7 +4,7 @@
 // @namespace       http://arimil.com
 // @description     Adds weaknesses to Pokemon Showdown tooltips.
 // @license         Creative Commons Attribution License
-// @version         0.1.3
+// @version         0.2
 // @include         http://play.pokemonshowdown.com/*
 // @require         http://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js
 // ==/UserScript==
@@ -131,7 +131,7 @@ function weakness()
         var index = this.weaknesses.indexOf(r);
         if(index != -1)
         {
-        	this.weaknesses.splice(index, 1);
+            this.weaknesses.splice(index, 1);
         }
     };
     this.setAttributes = function(attr){
@@ -149,67 +149,90 @@ function outputWeaknesses()
     $('.tooltip').find('h2').find('img').each(function(index, match) {
         console.log(match);
         var atr = $(match).attr("alt");
-        switch (atr)
-        {
-            case "Normal":
-                weak.setAttributes(new normal);
-                break;
-            case "Fire":
-                weak.setAttributes(new fire);
-                break;
-            case "Water":
-                weak.setAttributes(new water);
-                break;
-            case "Electric":
-                weak.setAttributes(new electric);
-                break;
-            case "Grass":
-                weak.setAttributes(new grass);
-                break;
-            case "Ice":
-                weak.setAttributes(new ice);
-                break;
-            case "Fighting":
-                weak.setAttributes(new fighting);
-                break;
-            case "Poison":
-                weak.setAttributes(new poison);
-                break;
-            case "Ground":
-                weak.setAttributes(new ground);
-                break;
-            case "Flying":
-                weak.setAttributes(new flying);
-                break;
-            case "Psychic":
-                weak.setAttributes(new psychic);
-                break;
-            case "Bug":
-                weak.setAttributes(new bug);
-                break;
-            case "Rock":
-                weak.setAttributes(new rock);
-                break;
-            case "Ghost":
-                weak.setAttributes(new ghost);
-                break;
-            case "Dragon":
-                weak.setAttributes(new dragon);
-                break;
-            case "Dark":
-                weak.setAttributes(new dark);
-                break;
-            case "Steel":
-                weak.setAttributes(new steel);
-                break;
-            case "Fairy":
-                weak.setAttributes(new fairy);
-                break;
-            default:
-                alert("Unknown Type: " + atr);
-                break;
-        }
+        addWeaknessByName(weak, atr);
     });
+    //add the images
+    $('.tooltip').find('h2').append('<br>');
+    var weaknesses = calculateWeaknesses(weak).getWeaknesses();
+    for(i = 0; i < weaknesses.length; i++)
+    {
+        var imgstring = "//play.pokemonshowdown.com/sprites/types/"+weaknesses[i]+".png";
+        imgstring = '<img src="'+imgstring+'" alt="'+weaknesses[i]+'" height="14" width="32"';
+        if (i > 0)
+        {
+            imgstring += ' class="b"';
+        }
+        imgstring += '>';
+        $('.tooltip').find('h2').append(imgstring);
+    }
+}
+
+function addWeaknessByName(weak, atr)
+{
+    switch (atr)
+    {
+        case "Normal":
+            weak.setAttributes(new normal);
+            break;
+        case "Fire":
+            weak.setAttributes(new fire);
+            break;
+        case "Water":
+            weak.setAttributes(new water);
+            break;
+        case "Electric":
+            weak.setAttributes(new electric);
+            break;
+        case "Grass":
+            weak.setAttributes(new grass);
+            break;
+        case "Ice":
+            weak.setAttributes(new ice);
+            break;
+        case "Fighting":
+            weak.setAttributes(new fighting);
+            break;
+        case "Poison":
+            weak.setAttributes(new poison);
+            break;
+        case "Ground":
+            weak.setAttributes(new ground);
+            break;
+        case "Flying":
+            weak.setAttributes(new flying);
+            break;
+        case "Psychic":
+            weak.setAttributes(new psychic);
+            break;
+        case "Bug":
+            weak.setAttributes(new bug);
+            break;
+        case "Rock":
+            weak.setAttributes(new rock);
+            break;
+        case "Ghost":
+            weak.setAttributes(new ghost);
+            break;
+        case "Dragon":
+            weak.setAttributes(new dragon);
+            break;
+        case "Dark":
+            weak.setAttributes(new dark);
+            break;
+        case "Steel":
+            weak.setAttributes(new steel);
+            break;
+        case "Fairy":
+            weak.setAttributes(new fairy);
+            break;
+        default:
+            cosnole.log("Unknown Type: " + atr);
+            break;
+    }
+}
+
+function calculateWeaknesses(weak)
+{
     console.log("PokeWeakness: Adding all weaknesses");
     var types = weak.getAttributes();
     //add all weaknesses for all types
@@ -241,41 +264,69 @@ function outputWeaknesses()
             weak.remove(immunity[im]);
         }
     }
-    //add the images
-    $('.tooltip').find('h2').append('<br>');
-    var weaknesses = weak.getWeaknesses();
-    for(i = 0; i < weaknesses.length; i++)
-    {
-        var imgstring = "//play.pokemonshowdown.com/sprites/types/"+weaknesses[i]+".png";
-        imgstring = '<img src="'+imgstring+'" alt="'+weaknesses[i]+'" height="14" width="32"';
-        if (i > 0)
-        {
-            imgstring += ' class="b"';
-        }
-        imgstring += '>';
-        $('.tooltip').find('h2').append(imgstring);
-    }
+    return weak;
+}
+
+function updateTeambuilder()
+{
+    //teambuilder weaknesses
+    $('.teambuilder-results').find('.result').each(function(index, match){
+        var tweak = new weakness;
+        $(match).find('.typecol').find('img').each(function(i, img){
+            var atr = $(img).attr('alt');
+            addWeaknessByName(tweak, atr);
+        });
+        $(match).find('a').append('<span class="col weakcol"></span>');
+        tweak = calculateWeaknesses(tweak);
+        $.each(tweak.getWeaknesses(), function(i, weak){
+            var imgstring = "//play.pokemonshowdown.com/sprites/types/"+weak+".png";
+            imgstring = '<img src="'+imgstring+'" alt="'+weak+'" height="14" width="32">';
+            $(match).find('.weakcol').append(imgstring);
+        });
+    });
 }
 
 $( document ).ready(function() {
-    var found = false;
-    var observer;
-    var bodyobserver = new MutationObserver(function(mutations, observer) {
-        if (document.getElementById('tooltipwrapper') && !found)
+    var teambuilder = false;
+    var tooltipobserver;
+    var tabobserver;
+    var teamobserver;
+    var resultobserver;
+    //add the tooltip wrapper div so we don't have to watch the whole page for updates
+    $('body').first().append('<div id="tooltipwrapper"></div>');
+    //bind the tooltipobserver to the tooltip div
+    tooltipobserver = new MutationObserver(function(mutation, observer) {
+        outputWeaknesses();
+    });
+    tooltipobserver.observe($('#tooltipwrapper')[0], {
+        childList: true
+    });
+    //mutation observer to teambuilder tab
+    teamobserver = new MutationObserver(function(mutation, observer) {
+    	if($('.teambuilder-results').length)
         {
-            console.log("PokeWeakness: Found tooltipwrapper");
-            found = true;
-            outputWeaknesses();
-            observer = new MutationObserver(function(mutation, observer) {
-                outputWeaknesses();
-            });
-            observer.observe(document.getElementById('tooltipwrapper'), {
+            resultobserver.observe($('.teambuilder-results')[0], {
                 childList: true
             });
-            bodyobserver.disconnect();
         }
     });
-    bodyobserver.observe(document.getElementsByTagName('body')[0], {
-        childList: true
+    //mutation observer for results pane
+    resultobserver = new MutationObserver(function(mutation, observer) {
+    	updateTeambuilder();
+    });
+    //watch the maintab for updates
+    tabobserver = new MutationObserver(function(mutation, observer) {
+    	//check if teambuilder was opened
+        if($('.teambuilder-clipboard-container').length)
+        {
+            //bind a mutationobserver to the tabs content area
+            teamobserver.observe($('.teambuilder-clipboard-container').parent().parent()[0], {
+                childList: true
+            });
+        }
+    });
+    tabobserver.observe($('.maintabbar')[0], {
+        childList: true,
+        subtree: true
     });
 });
